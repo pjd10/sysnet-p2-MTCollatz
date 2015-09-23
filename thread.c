27a0a162541496544f5b2decb1ea3aacb391622e
pjd10 <pjd10@students.uwf.edu>
2015-09-23 13:40:33 -0500

    Create thread.c

---
+++ thread.c
@@ -0,0 +1,13 @@
+/** @file       thread.c
+ *  @brief      Implements the functions of thread.h.
+ *
+ *  @author     Paul Davila (pjd10)
+ *  @author     Christopher Blaylock (cdb56)
+ *  @class      COP4634
+ *  @assignment Project 2
+ *
+ *  @bug        No known bugs
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
