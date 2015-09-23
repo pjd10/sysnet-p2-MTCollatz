46cf95dfbcac29460c405a019198642378005947
pjd10 <pjd10@students.uwf.edu>
2015-09-23 13:39:26 -0500

    Create mt-collatz.c

---
+++ mt-collatz.c
@@ -0,0 +1,13 @@
+/** @file       mt-collatz.c
+ *  @brief      Main file for the program.
+ *
+ *  @author     Paul Davila (pjd10)
+ *  @author     Christopher Blaylock (cdb56)
+ *  @class      COP4634
+ *  @assignment Project 2
+ *
+ *  @bug        No known bugs
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
