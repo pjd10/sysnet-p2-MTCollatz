eb8cecb91ddd5ee325d617d67f8702b1ee099e18
pjd10 <pjd10@students.uwf.edu>
2015-09-24 01:49:21 -0500

    Update thread.c

--- thread.c
+++ thread.c
@@ -1,5 +1,5 @@
 /** @file       thread.c
- *  @brief      Implements the functions of thread.h.
+ *  @brief      File that implements the fuctions of thread.h
  *
  *  @author     Paul Davila (pjd10)
  *  @author     Christopher Blaylock (cdb56)
@@ -9,6 +9,30 @@
  *  @bug        No known bugs
  */
 
+ // Library Includes
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
 #include "thread.h"
+
+int collatzSequence(unsigned long int n, int i)
+{
+	if(n == 1)
+		return i;
+
+	else if ((n % 2) == 0)
+	{
+		n = n / 2;
+		i++;
+		return collatzSequence(n, i);
+	}
+	else if((n % 2) != 0)
+	{
+		n = 3 * n + 1;
+		i++;
+		return collatzSequence(n, i);
+	}
+
+	return i;	
+}
